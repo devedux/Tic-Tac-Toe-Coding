3aaa6f3b20a5b8ecb4a9f6d6611aa5158f2fb582
devedux <devedux@gmail.com>
2021-11-29 07:28:15 -0500

    Calculate winner, nextvalue and status for game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import './App.css';
+import { useLocalStorageState } from './utils';
 
+type SquaresType = any[]
 type BoardType = {
-  squares: any[], 
-  selectSquare: (step: number) => void
+  squares: SquaresType, 
+  onClick: (i: number) => void
 }
-function Board({squares, selectSquare}: BoardType) {
+function Board({squares, onClick}: BoardType) {
   function renderSquare(i: number) {
     return (
-      <button className='board-square' onClick={() => selectSquare(i)}>
+      <button className='board-square' onClick={() => onClick(i)}>
         {squares[i]}
       </button>
     )
@@ -35,10 +37,32 @@ function Board({squares, selectSquare}: BoardType) {
 }
 
 function Game() {
+  const [squares, setSquares] = useLocalStorageState('tic-tac-toe:squares', Array(9).fill(null))
+
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
+  function selectSquare(square: number) {
+    if (winner || squares[square]) {
+      return
+    }
+
+    const squaresCopy = [...squares]
+    squaresCopy[square] = nextValue
+    setSquares(squaresCopy)
+  }
+
   return (
-    <div className='game'>
-      <Board squares={[]} selectSquare={(step) => console.log(step)} />
+    <div>
+      <div>
+        {status}
+      </div>
+      <div className='game'>
+        <Board squares={squares} onClick={selectSquare} />
+      </div>
     </div>
+
   )
 }
 
@@ -48,6 +72,39 @@ function App() {
       <Game />
     </div>
   );
+}
+
+function calculateNextValue(squares: SquaresType) {
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : '0'
+}
+
+function calculateWinner(squares: SquaresType) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]  
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+type WinnerType = string | null
+function calculateStatus(winner: WinnerType, squares: SquaresType, nextValue: string) {
+  return winner
+    ? `Winner: ${winner}`
+    : squares.every(Boolean)
+    ? `Scratch: Cat's game`
+    : `Next player: ${nextValue}`
 }
 
 export default App;
